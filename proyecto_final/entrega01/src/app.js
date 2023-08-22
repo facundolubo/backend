c493cc382e87e3fa61ee62f5652426628e7e1cda
Facundo <facundolubo@gmail.com>
2023-08-22 09:38:40 -0300

    products router

--- proyecto_final/entrega01/src/app.js
+++ proyecto_final/entrega01/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import {productsRouter} from './routers/products.router.js'
 import {ProductManager} from './ProductManager.js'
 
 const users =[{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }, { id: 3, name: 'Jack Doe' }]
@@ -7,14 +8,7 @@ const app = express()
 
 const productManager = new ProductManager('./products.json')
 
-app.get('/products', async (req, res) => {
-    res.status(200).json({status: 'ok', data: await productManager.getProducts()})
-})
-app.get('/products/:id', async (req, res) => {
-    const result = users.find(user => user.id === parseInt(req.params.id))
-    if (result) res.status(200).json({status: 'ok', data: result})
-    else res.status(404).json({status: 'error', message: 'User not found'})
-})
+
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
