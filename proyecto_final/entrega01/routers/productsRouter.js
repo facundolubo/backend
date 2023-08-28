2cdac12454cd8df44834272220cb44d293504411
Facundo <facundolubo@gmail.com>
2023-08-28 17:46:29 -0300

    Products instead of Users

--- proyecto_final/entrega01/routers/productsRouter.js
+++ proyecto_final/entrega01/routers/productsRouter.js
@@ -11,14 +11,14 @@ router.get('/', async (req, res) => {
     const limit = req.query.limit
     if (typeof result === 'string') {
         const error = result.split(' ')
-        return res.status(400).json({status: 'error', message: error[0]})
+        return res.status(400).json({status: 'Error', message: error[0]})
     }
     res.status(200).json({status: 'ok', data: result.slice(0, limit)})
 })
 router.get('/:id', async (req, res) => {
-    const result = users.find(user => user.id === parseInt(req.params.id))
+    const result = productManager.getProductById(req.params.id)
     if (result) res.status(200).json({status: 'ok', data: result})
-    else res.status(404).json({status: 'error', message: 'User not found'})
+    else res.status(404).json({status: 'Error', message: 'Product not found'})
 })
 
 export default router
