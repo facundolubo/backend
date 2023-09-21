d1ff30fd5c3f1f0bcdb7a8a7fb0131a655de9efe
Facundo <facundolubo@gmail.com>
2023-09-21 15:47:03 -0300

    Conexión con mongo

--- entrega06/public/js/index.js
+++ entrega06/public/js/index.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose'
 
+/* read user and password securely*/
+
+const fs = await import('fs');
+const fileContents = await fs.readFileSync('../../../mongo/auth.txt', 'utf8');
+const USER = fileContents.split('\n')[0]
+const PASSWORD = fileContents.split('\n')[1]
+
 const productsSchema = new mongoose.Schema({
     title: String,
     description: String,
@@ -10,3 +17,15 @@ const productsSchema = new mongoose.Schema({
     stock: Number,
     id: Number
 })
+
+const productsDAO = mongoose.model('products', productsSchema)
+
+try {
+    await mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.jv8xqu9.mongodb.net/`)
+    console.log('Connected')
+} catch (error) {
+    console.log(error.message)
+    console.log("Se intentó loguear con user " + USER + " y password " + PASSWORD)
+}
+
+export {productsDAO }
