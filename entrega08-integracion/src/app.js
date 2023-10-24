39c98a93cfd5ac0380f731b0875853e043fe2a32
Facundo Lubo <facundolubo@gmail.com>
2023-10-24 15:11:29 -0300

    cookiesparser

--- entrega08-integracion/src/app.js
+++ entrega08-integracion/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
+app.use(cookieParser());
 app.get("/", (req, res) => {
     res.cookie('oreo', 'chocolate').send('cookie set');
 })
